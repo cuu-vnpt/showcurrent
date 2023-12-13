73740f796cf347a4f5c6c999f43a87c07701c858
cuu-vnpt <kunnobita97@gmail.com>
2023-12-14 00:09:05 +0700

    newfile

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace UsingCurrent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap ty gia: ");
+            float usd = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so USD ban co: ");
+            float myUSD = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so tien viet ban co sau khi doi la: " + usd * myUSD);
+        }
+    }
+}
